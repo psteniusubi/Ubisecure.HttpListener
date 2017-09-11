e22f72f297b09ac6a7db9b9decfd3bf4887bc744
Petteri Stenius <petteri.stenius@ubisecure.com>
2017-09-11 12:08:33 +0300

    update

--- http-listener.ps1
+++ http-listener.ps1
@@ -1,20 +1,27 @@
 ﻿[CmdletBinding()]
 param()
 
-#$files = Get-ChildItem -Path "C:\Users\jeps\source\repos\SimpleHttpListener\SimpleHttpListener" -Filter "*.cs" | Select-Object -ExpandProperty FullName
-#$type = Add-Type -Path $files -PassThru
-#Import-Module -Assembly $type.Assembly -Prefix "Http" -Force
-
-#Import-Module -Name (Split-Path -Path $PSCommandPath -Parent | Join-Path -ChildPath "bin/Debug/net47/HttpListener.dll") -Prefix "Http" -Force
-Import-Module "HttpListener" #-Verbose
+Import-Module "HttpListener" 
 
 $listener = Start-HttpListener -Prefix "http://localhost:12340/hello/" -RandomPort 
-Write-Host $listener.Prefix
+
+$b = [uribuilder]::new($listener.Prefix)
+$b.Path = "/hello/begin"
+
+$completed = @"
+<body onload="window.close()">
+<p>The operation was completed.</p>
+<p><input type="button" onclick="window.close()" value="Close"></input></p>
+</body>
+"@
+
+Start-Process -Verb "open" -FilePath $b.Uri | Out-Null
+
 $req = $listener | Read-HttpRequest | % {
     $local:r = $_
 	switch($r.Url.LocalPath) {
-		"/hello/" { $r | Write-HttpResponse -Location "/hello/world" }
-		"/hello/world" { $r | Write-HttpResponse -Body "<p>hello</p>" -Stop -PassThru }
+		"/hello/begin" { $r | Write-HttpResponse -Location "/hello/" }
+		"/hello/" { $r | Write-HttpResponse -Body $completed -Stop -PassThru }
 	}
 }
 $listener | Stop-HttpListener
