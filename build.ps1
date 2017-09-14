9a3f8db5f2d7de9aab59f190278367453b103987
Petteri Stenius <petteri.stenius@ubisecure.com>
2017-09-14 11:47:02 +0300

    fixes

--- build.ps1
+++ build.ps1
@@ -1,8 +1,11 @@
 [CmdletBinding()]
 param()
 
-$module = Split-Path -Path $PSCommandPath -Parent | Join-Path -ChildPath "bin/Debug/net47/HttpListener.dll"
+$project = Join-Path -Path $PSScriptRoot -ChildPath "HttpListener.csproj" -Resolve -ErrorAction Stop
+$module = Join-Path -Path $PSScriptRoot -ChildPath "bin/Debug/net47/HttpListener.dll"
 
+Write-Verbose "Test-Path $module"
 if(-not (Test-Path -Path $module)) {
-    & dotnet build -f net47 -v q | Out-Null
+    Write-Verbose "dotnet build $project"
+    & dotnet build $project -f net47 -v q | Out-Null
 }
