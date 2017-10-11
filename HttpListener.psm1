8426aab77d08823f27a5a0cc15bed388e2f2098e
Petteri Stenius <petteri.stenius@ubisecure.com>
2017-10-11 11:58:35 +0300

    updates

--- HttpListener.psm1
+++ HttpListener.psm1
@@ -1,12 +1,3 @@
-if($false) {
-    $local:module = New-Module -Name "HttpListener" -ScriptBlock {
-        $local:cs = Get-ChildItem -Path $PSScriptRoot -Filter "*.cs" | Select-Object -ExpandProperty "FullName" | Sort-Object
-        $local:dll = Add-Type -Path $local:cs -PassThru -ErrorAction Stop | Select-Object -ExpandProperty "Assembly" -Unique
-        Import-Module -Assembly $local:dll -Prefix "Http" -Cmdlet "Start-Listener","Read-Request","Write-Response","Stop-Listener" -ErrorAction Stop 
-    }
-    Import-Module $local:module 
-} else {
-    $local:cs = Get-ChildItem -Path $PSScriptRoot -Filter "*.cs" | Select-Object -ExpandProperty "FullName" | Sort-Object
-    $local:dll = Add-Type -Path $local:cs -PassThru -ErrorAction Stop | Select-Object -ExpandProperty "Assembly" -Unique
-    Import-Module -Assembly $local:dll -Prefix "Http" -Cmdlet "Start-Listener","Read-Request","Write-Response","Stop-Listener" -ErrorAction Stop 
-}
+$local:cs = Get-ChildItem -Path $PSScriptRoot -Filter "*.cs" | Select-Object -ExpandProperty "FullName" | Sort-Object
+$local:dll = Add-Type -Path $local:cs -PassThru -ErrorAction Stop | Select-Object -ExpandProperty "Assembly" -Unique
+Import-Module -Assembly $local:dll -Prefix "Http" -Cmdlet "Start-Listener","Read-Request","Write-Response","Stop-Listener" -ErrorAction Stop 
